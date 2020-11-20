ef39597e3d1ece762e7170862d6715bb7c726468
zhouqi <17784574@qq.com>
2020-11-20 17:29:29 +0800

    补充 DDL

---
+++ shudong.sql
@@ -0,0 +1,108 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80014
+ Source Host           : localhost:3306
+ Source Schema         : shudong
+
+ Target Server Type    : MySQL
+ Target Server Version : 80014
+ File Encoding         : 65001
+
+ Date: 20/11/2020 17:28:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_uaa_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `t_uaa_authority`;
+CREATE TABLE `t_uaa_authority`  (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限名称',
+  `mark` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '权限标识',
+  `disabled` int(1) NOT NULL COMMENT '是否禁用   1:是  0:否',
+  `create_user_id` bigint(20) NOT NULL COMMENT '创建人',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_user_id` bigint(20) NOT NULL COMMENT '修改人',
+  `update_time` datetime(0) NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_app_mark`(`mark`) USING BTREE COMMENT '权限标识唯一索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_uaa_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_uaa_role`;
+CREATE TABLE `t_uaa_role`  (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
+  `mark` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色标识',
+  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '角色描述',
+  `disabled` int(1) NOT NULL COMMENT '是否禁用   1:是  0:否',
+  `create_user_id` bigint(20) NOT NULL COMMENT '创建人',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_user_id` bigint(20) NOT NULL COMMENT '修改人',
+  `update_time` datetime(0) NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_mark`(`name`) USING BTREE COMMENT '角色标识唯一索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_uaa_role_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `t_uaa_role_authority`;
+CREATE TABLE `t_uaa_role_authority`  (
+  `id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL COMMENT '角色 id',
+  `authority_id` bigint(20) NOT NULL COMMENT '权限 id',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_roleid_authorityid`(`role_id`, `authority_id`) USING BTREE COMMENT '角色id和权限id唯一索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色权限关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_uaa_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_uaa_user`;
+CREATE TABLE `t_uaa_user`  (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `disabled` int(1) NOT NULL COMMENT '是否禁用 1:是 0:否',
+  `deleted` int(1) NOT NULL COMMENT '是否删除 1:是  0:否',
+  `create_user_id` bigint(20) NOT NULL COMMENT '创建人 , 若为自行注册的用户创建人为其自己',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_user_id` bigint(20) NOT NULL COMMENT ' 最后修改人',
+  `update_time` datetime(0) NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_username`(`username`) USING BTREE COMMENT '用户名唯一索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_uaa_user_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `t_uaa_user_authority`;
+CREATE TABLE `t_uaa_user_authority`  (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL COMMENT '用户主键 ID',
+  `authority_id` bigint(20) NOT NULL COMMENT '权限主键 ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_uaa_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_uaa_user_role`;
+CREATE TABLE `t_uaa_user_role`  (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL COMMENT '用户 id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色 id',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_userid_roleid`(`user_id`, `role_id`) USING BTREE COMMENT '用户id和角色id唯一索引'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色关联表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
